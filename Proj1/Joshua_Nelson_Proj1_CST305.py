a52a8745f4b593223c210650d5c76e7b5939f4f0
Josh Nelson <joshuaknelson1202@icloud.com>
2025-09-12 11:52:55 -0700

    initial commit

---
+++ Proj1/Joshua_Nelson_Proj1_CST305.py
@@ -0,0 +1,26 @@
+import matplotlib
+import matplotlib.pyplot as plt 
+from scipy.integrate import odeint 
+import numpy as np 
+
+# ODE for battery charging
+# dQ/dt = (Qmax - Q) / (Resistance*Capacitance (AKA RC))
+def dQ_dt(t, Q, Qmax=100, RC=5):
+    return (Qmax - Q) / RC
+
+# Initial charge (battery starts at 0%)
+Q0 = 0
+
+# Time range (e.g., 0 to 50 units)
+t = np.linspace(0, 50, 200)
+
+# Solve ODE
+Q = odeint(dQ_dt, Q0, t, tfirst=True)
+
+# Plot solution
+plt.plot(t, Q, label="Battery Charge")
+plt.xlabel('Time', fontsize=22)
+plt.ylabel('Charge (%)', fontsize=22)
+plt.title("Battery Charging Curve", fontsize=18)
+plt.legend()
+plt.show()
